0d355c8e1d6e25dc3467a0d8efe34b5392b99d57
Daniel Joji Koons <jojikoons@gmail.com>
2019-10-25 18:48:08 -0700

    Finishing Javascript File

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     
     var x = $("#number");
     var y = $("#guessed");
+    var z = $("#wins")
+    var a = $("#lose")
 
     var number = Math.floor(Math.random() * (120 - 19) + 19);
     var counter = 0;
@@ -11,10 +13,13 @@ $(document).ready(function() {
     var blue = Math.floor(Math.random() * 11) + 1;
     var green = Math.floor(Math.random() * 11) + 1;
     var yellow = Math.floor(Math.random() * 11) + 1;
-
+    var wins = 0;
+    var losses = 0;
 
     x.text(number);
     y.text(counter);
+    z.text(wins)
+    a.text(losses)
 
         // number = Math.floor(Math.random() * (120 - 19) + 19);
         // x.val(number)
@@ -24,50 +29,59 @@ $(document).ready(function() {
 
 function restart()
 {
-        console.log(y.val())
-        var number = Math.floor(Math.random() * (120 - 19) + 19);
-        var red = Math.floor(Math.random() * 11) + 1;
-        var blue = Math.floor(Math.random() * 11) + 1;
-        var green = Math.floor(Math.random() * 11) + 1;
-        var yellow = Math.floor(Math.random() * 11) + 1;
+         console.log(y.val())
+         number = Math.floor(Math.random() * (120 - 19) + 19);
+         red = Math.floor(Math.random() * 11) + 1;
+         blue = Math.floor(Math.random() * 11) + 1;
+         green = Math.floor(Math.random() * 11) + 1;
+         yellow = Math.floor(Math.random() * 11) + 1;
 
         counter = 0;
-        y.val(0)
-        x.val(number)
+        x.text(number);
+        y.text(counter);
 }
 
 function winGame(){
     alert("U win")
-    restart()
+    wins++
+    z.text(wins)
+    
 }
 
 function loseGame(){
     alert("u lose")
-    restart()
+    losses++
+    a.text(losses)
 }
-
-        
 
         $("#red").on("click", function() {
             if(counter != number){
                 counter += red;
                 y.text(counter);
+                if(counter > number){
+                    loseGame()
+                    restart()
+                }
+                else if (counter == number){
+                    winGame()
+                    restart()
+                }   
             }
-            else if(counter > number){
-                loseGame()
-            }
-            else{
-                winGame()
-            }
+
         });
 
         $("#blue").on("click", function() {
             if(counter != number){
                 counter += blue;
                 y.text(counter);
-            }
-            else{
-                restart()
+                if(counter > number){
+                    loseGame()
+                    restart()
+                }
+                else if (counter == number){
+                    winGame()
+                    restart()
+                }   
             }
         });
     
@@ -75,9 +89,14 @@ function loseGame(){
             if(counter != number){
                 counter += green;
                 y.text(counter);
-            }
-            else{
-                restart()
+                if(counter > number){
+                    loseGame()
+                    restart()
+                }
+                else if (counter == number){
+                    winGame()
+                    restart()
+                }   
             }
         });
     
@@ -85,9 +104,14 @@ function loseGame(){
             if(counter != number){
                 counter += yellow;
                 y.text(counter);
-            }
-            else{
-                restart()
+                if(counter > number){
+                    loseGame()
+                    restart()
+                }
+                else if (counter == number){
+                    winGame()
+                    restart()
+                }   
             }
         });
   });
